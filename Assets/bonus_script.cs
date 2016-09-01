07a416f3256ff8e1b73ff504d8c4733980b93422
Мезенцев Иван <MezencevIP@npf-isb.ru>
2016-09-01 23:44:06 +0700

    audio, фикс ящиков и ябло, небольшогй фикс прыжков

---
+++ Assets/bonus_script.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class bonus_script : MonoBehaviour
+{
+    
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<AudioSource>().Play();
+            Destroy(gameObject);
+        }
+    }
+}
